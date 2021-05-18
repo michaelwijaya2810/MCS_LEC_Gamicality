ac2b6b53640862b06fc62406bd1ffd32fb6d4226
michaelwijaya2810 <michaelwijaya.mw20@gmail.com>
2021-05-18 20:41:51 +0700

    Added appbar template for profile button & option menu

--- app/src/main/java/com/mcs_lec_project/gamicality/MainActivity.java
+++ app/src/main/java/com/mcs_lec_project/gamicality/MainActivity.java
@@ -1,8 +1,12 @@
 package com.mcs_lec_project.gamicality;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.action_profile){
+            return true;
+        }else if(item.getItemId() == R.id.menu_home){
+            return true;
+        }else if(item.getItemId() == R.id.menu_game_list){
+            return true;
+        }else if(item.getItemId() == R.id.menu_bookmarks){
+            return true;
+        }else if(item.getItemId() == R.id.menu_notif){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

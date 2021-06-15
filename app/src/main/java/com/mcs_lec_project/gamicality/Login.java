25ade6b6954d2378a8272310d89596cf6045844a
riksensetiawan <@riksensetiawan>
2021-06-15 17:16:52 +0700

    login regis done

--- app/src/main/java/com/mcs_lec_project/gamicality/Login.java
+++ app/src/main/java/com/mcs_lec_project/gamicality/Login.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -49,6 +50,7 @@ public class Login extends AppCompatActivity {
                     {
                         Toast.makeText(Login.this, "Login Success", Toast.LENGTH_SHORT).show();
 
+
                         // intent after login to HOME
                         /*
                         Intent intent = new Intent(Login.this,Home.class);
@@ -67,6 +69,7 @@ public class Login extends AppCompatActivity {
 
                 }
                 Toast.makeText(Login.this, "User not found", Toast.LENGTH_SHORT).show();
+
             }
         });
 
--- app/src/main/java/com/mcs_lec_project/gamicality/Register.java
+++ app/src/main/java/com/mcs_lec_project/gamicality/Register.java
@@ -3,6 +3,7 @@ package com.mcs_lec_project.gamicality;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -56,6 +57,8 @@ public class Register extends AppCompatActivity {
                 else
                 {
                     dbhandler.insertuser(username,password,email);
+                    Intent intent = new Intent(Register.this,Login.class);
+                    startActivity(intent);
                     Toast.makeText(context, "user created",Toast.LENGTH_SHORT);
                 }
             }
